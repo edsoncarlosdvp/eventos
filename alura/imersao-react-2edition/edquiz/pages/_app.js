2382bee74dd2ed9f46f14dc2605c61482d2b1c8c
edsoncarlosdvp <edsoncarlos@equipenovainfo.com.br>
2021-01-29 23:16:28 -0300

    Dia 01 => Concluído!

--- alura/imersao-react-2edition/edquiz/pages/_app.js
+++ alura/imersao-react-2edition/edquiz/pages/_app.js
@@ -2,10 +2,29 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import dbJson from '../db.json'
 
 const GlobalStyle = createGlobalStyle`
+
+  * {
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
 `
 
@@ -14,8 +33,8 @@ const theme = dbJson.theme
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
